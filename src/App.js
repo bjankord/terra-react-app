23efd7efa74cff659bd0e342fb1733affb3ef14e
Brett Jankord <brett.jankord@cerner.com>
2019-09-05 04:42:50 -0500

    Update with latest CRA changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Base from 'terra-base';
 import Button from 'terra-button';
 import Alert from 'terra-alert';
@@ -7,48 +7,41 @@ import './App.css';
 
 const locales = ['en', 'en-AU', 'en-US', 'en-GB', 'es', 'es-US', 'es-ES', 'de', 'fr', 'fr-FR', 'nl', 'nl-BE', 'pt', 'pt-BR', 'sv', 'sv-SE'];
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectLocale: 'en'
-    };
+function App() {
+  const [locale, setLocale] = useState('en');
 
-    this.handleSelectChange = this.handleSelectChange.bind(this);
-  }
-
-  handleSelectChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  render() {
-    return (
-      <Base locale={this.state.selectLocale}>
-        <div className="App">
-          <Alert type="success">Your Terra UI + Create React App works!</Alert>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <Button
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-              text="Learn React"
-              variant="emphasis"
-            />
-            <p>
-              <label htmlFor="locale-switcher">Select locale</label>
-              <select id="locale-switcher" className="locale-switcher" name="selectLocale" value={this.state.selectLocale} onChange={this.handleSelectChange}>
-                {locales.map((locale) => <option key={locale} value={locale}>{locale}</option>)}
-              </select>
-            </p>
-          </header>
-        </div>
-      </Base>
-    );
-  }
+  return (
+    <Base locale={locale}>
+      <div className="App">
+        <Alert type="success">Your Terra UI + Create React App works!</Alert>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Button
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            text="Learn React"
+            variant="emphasis"
+          />
+          <p>
+            <label htmlFor="locale-switcher">Select locale</label>
+            <select
+              id="locale-switcher"
+              className="locale-switcher"
+              name="selectLocale"
+              value={locale}
+              onChange={(event) => {setLocale(event.target.value)}}
+            >
+              {locales.map((item) => <option key={item} value={item}>{item}</option>)}
+            </select>
+          </p>
+        </header>
+      </div>
+    </Base>
+  );
 }
 
 export default App;
